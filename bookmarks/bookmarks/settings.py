78bede8a66a372f388fcfff1594589bdd2dd1798
Arbey Maya <vilmarbey@gmail.com>
2024-10-12 21:11:59 -0500

    pagina 303 libro

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from django.urls import reverse_lazy
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -40,9 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'easy_thumbnails',
     'social_django',
+    'actions.apps.ActionsConfig',
     'images.apps.ImagesConfig',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -157,3 +161,8 @@ SOCIAL_AUTH_PIPELINE = [
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
 ]
+
+ABSOLUTE_URL_OVERRIDES = {
+    'auth.user': lambda u: reverse_lazy('user_detail',
+                                        args=[u.username])
+}
--- env/bookmarks/Scripts/activate.bat
+++ env/bookmarks/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\arbey\Videos\ProyectoFormativo\ProyectoFormativo_V1\env\bookmarks
+set VIRTUAL_ENV=C:\Users\arbey\OneDrive\Imágenes\proyectoFormativo\ProyectoFormativo_V1\env\bookmarks
 
 if not defined PROMPT set PROMPT=$P$G
 
